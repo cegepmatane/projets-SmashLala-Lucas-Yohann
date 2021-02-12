e4c97a21757f2c9bf65ebb3b2048b9d23d06c56e
Lucas Guffroy <2032916@cgmatane.qc.ca>
2021-02-12 12:11:01 -0500

    #3 Prototype

---
+++ Doc/Project interface/Assets/BarreDeVie.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+/*
+public class HealthBar : MonoBehaviour
+{
+    public int maxVie = 100;
+    public int vieActuelle;
+
+    public HealthBar barredevie;
+
+    void Start()
+    {
+        vieActuelle = maxVie;
+        barredevie.SetMaxVie(maxVie);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        vieActuelle -= damage;
+        barredevie.SetVie(vieActuelle);
+    }
+}*/
