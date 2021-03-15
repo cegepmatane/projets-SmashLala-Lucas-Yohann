9ddcf6ba5f1f1020f82495e636dcd946229f265b
YohannVitale <31995754+YohannVitale@users.noreply.github.com>
2021-03-15 14:39:32 -0400

    #8 synchronization des la rotation
    Pour ce faire jai du mettre le scripte de deplacement dans le scripte JoueurReseau, afin que nous puissons envoyer au serveur la rotation des personnages

--- SmashLaLa/Assets/Monde/Script/JoueurReseau.cs
+++ SmashLaLa/Assets/Monde/Script/JoueurReseau.cs
@@ -3,14 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class JoueurReseau : MonoBehaviour
+public class JoueurReseau : MonoBehaviour, IPunObservable
 {
     public MonoBehaviour[] scripteAIgnorer;
     private PhotonView photonView;
+    private object IsFiring;
+    //public Deplacement deplacement;
+    public float vitesse;
+    public float jumpforce;
+
+    private bool isJumping;
+    private bool isGrounded;
+    private Vector3 velocity = Vector3.zero;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    public bool jeTourne;
+
     // Start is called before the first frame update
     void Start()
     {
-        //pour ignorer des scirpt (eviter le doublon)
+
+        
+        scripteAIgnorerF();
+    }
+
+    void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * vitesse * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        DeplacementJoueur(horizontalMovement);
+
+
+        Flip(rb.velocity.x);
+
+        float charactervelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("vitesse", charactervelocity);
+
+    }
+
+    public void DeplacementJoueur(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpforce));
+            isJumping = false;
+        }
+    }
+
+
+    public void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = true;
+            jeTourne = true;
+        }
+        else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = false;
+            
+        }
+    }
+
+    //pour ignorer des scirpt (eviter le doublon)
+    private void scripteAIgnorerF()
+    {
         photonView = GetComponent<PhotonView>();
         if (!photonView.IsMine)
         {
@@ -21,9 +98,26 @@ public class JoueurReseau : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-       // deplacement.DeplacementJoueur(float _horizontalMovement);
+        if (stream.IsWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(spriteRenderer.flipX);
+        }
+        else
+        {
+            // Network player, receive data
+            this.spriteRenderer.flipX = (bool)stream.ReceiveNext();
+        }
     }
+    
+
+
+
+ 
+
+
+
+    
 }
